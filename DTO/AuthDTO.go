06e478a2842f040d46eeaa56823a5a00a3da42b1
Nguyen-VuHa <havu20111999@gmail.com>
2024-07-02 20:42:09 +0700

    [update] - change snack key to camel key of json in DTO

--- DTO/AuthDTO.go
+++ DTO/AuthDTO.go
@@ -10,9 +10,9 @@ import (
 type SignUp_Request struct {
 	Email       string `json:"email"` // trả ra JSON với định dạng email thay vì Email
 	Password    string `json:"password"`
-	FullName    string `json:"full_name"`
-	BirthDay    string `json:"birth_day"`
-	PhoneNumber string `json:"phone_number"`
+	FullName    string `json:"fullName"`
+	BirthDay    string `json:"birthDay"`
+	PhoneNumber string `json:"phoneNumber"`
 	Address     string `json:"address"`
 }
 
@@ -24,15 +24,15 @@ type AuthService_SignUp_Response struct {
 type SignIn_Request struct {
 	Email     string `json:"email"` // trả ra JSON với định dạng email thay vì Email
 	Password  string `json:"password"`
-	IPAddress string `json:"ip_address"`
+	IPAddress string `json:"ipAddress"`
 	Device    string `json:"device"`
 }
 
 // Type request sign up account
 type AuthService_SignIn_Response struct {
-	UserID       uuid.UUID `json:"u_id"`
-	AccessToken  string    `json:"acc_k"`
-	RefreshToken string    `json:"ref_k"`
+	UserID       uuid.UUID `json:"uid"`
+	AccessToken  string    `json:"ak"`
+	RefreshToken string    `json:"fk"`
 }
 
 // Type request sign up with facebook
@@ -49,14 +49,14 @@ type Callback_SignIn_Facebook struct {
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
-	AccessToken string    `json:"access_token"`
-	TokenType   string    `json:"token_type"`
+	AccessToken string    `json:"accessToken"`
+	TokenType   string    `json:"tokenType"`
 	Expiry      time.Time `json:"expiry"`
 }
 
 // Type request sign up account
 type AuthService_Callback_Facebook_Response struct {
-	UserID      uuid.UUID `json:"u_id"`
-	AccessToken string    `json:"acc_k"`
+	UserID      uuid.UUID `json:"aid"`
+	AccessToken string    `json:"ak"`
 	Method      uint      `json:"method"`
 }
